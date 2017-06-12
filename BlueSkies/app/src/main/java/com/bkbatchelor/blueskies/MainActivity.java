a5c54230f3a7542ab73204673fced8a62d1d01e6
Brian Batchelor <batchbk@gmail.com>
2017-06-12 18:01:13 -0400

    updated slide and add project

---
+++ BlueSkies/app/src/main/java/com/bkbatchelor/blueskies/MainActivity.java
@@ -0,0 +1,40 @@
+package com.bkbatchelor.blueskies;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
