b3239f3ce8e9fd06f60b5759cd183adbcaca645c
Brian Batchelor <batchbk@gmail.com>
2017-06-13 16:45:52 -0400

    completed activiy

--- BlueSkies/app/src/main/java/com/bkbatchelor/blueskies/MainActivity.java
+++ BlueSkies/app/src/main/java/com/bkbatchelor/blueskies/MainActivity.java
@@ -5,12 +5,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
     }
 
     @Override
