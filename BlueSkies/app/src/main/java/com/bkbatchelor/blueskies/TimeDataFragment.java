018623a6ac8c8eac2769d8474a556be93a714027
Brian Batchelor <batchbk@gmail.com>
2017-06-15 18:18:05 -0400

    completed first draft

--- BlueSkies/app/src/main/java/com/bkbatchelor/blueskies/TimeDataFragment.java
+++ BlueSkies/app/src/main/java/com/bkbatchelor/blueskies/TimeDataFragment.java
@@ -1,10 +1,17 @@
 package com.bkbatchelor.blueskies;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-/**
- * Created by Brian.Batchelor on 6/15/2017.
- */
 
 public class TimeDataFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.time_date_frag, parent, false);
+    }
 }
