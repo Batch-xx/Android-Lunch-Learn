a98680d482f159e5c5d55b6184c959aa52fe6622
Brian Batchelor <batchbk@gmail.com>
2017-06-15 17:49:14 -0400

    completed first draft

---
+++ BlueSkies/app/src/main/java/com/bkbatchelor/blueskies/TemperatureFragment.java
@@ -0,0 +1,10 @@
+package com.bkbatchelor.blueskies;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Brian.Batchelor on 6/15/2017.
+ */
+
+public class TemperatureFragment extends Fragment {
+}
